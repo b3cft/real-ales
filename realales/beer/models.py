3d0e556af620b911a214364def75fa5a05fbe725
Andrew "Bob" Brockhurst <andy.brockhurst@b3cft.com>
2011-05-18 23:43:38 +0100

    update to model validation

--- realales/beer/models.py
+++ realales/beer/models.py
@@ -4,9 +4,9 @@ from brewery.models import Brewery
 class Beer(models.Model):
     name    = models.CharField(max_length=200) 
     brewery = models.ForeignKey(Brewery)
-    notes   = models.TextField(null=True)
-    abv     = models.FloatField(null=True)
-    og      = models.IntegerField(null=True)
+    notes   = models.TextField(null=True, blank=True)
+    abv     = models.FloatField(null=True, blank=True)
+    og      = models.IntegerField(null=True, blank=True)
     
     def __unicode__(self):
         return self.name
--- realales/brewery/models.py
+++ realales/brewery/models.py
@@ -3,13 +3,13 @@ from region.models import Region
     
 class Brewery(models.Model):
     name        = models.CharField(max_length=200)
-    location    = models.CharField(max_length=200, null=True)
-    address     = models.TextField(null=True)
-    postcode    = models.CharField(max_length=10, null=True)
-    region      = models.ForeignKey(Region, null=True)
-    lat         = models.FloatField(null=True)
-    long        = models.FloatField(null=True)
-    description = models.TextField(null=True)
+    location    = models.CharField(max_length=200, null=True, blank=True)
+    address     = models.TextField(null=True, blank=True)
+    postcode    = models.CharField(max_length=10, null=True, blank=True)
+    region      = models.ForeignKey(Region, null=True, blank=True)
+    lat         = models.FloatField(null=True, blank=True)
+    long        = models.FloatField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
     active      = models.BooleanField()
 
     def __unicode__(self):
