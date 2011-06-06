657dffe9a9d534427ac5afba083c6d8085c585a1
Andrew "Bob" Brockhurst <andy.brockhurst@b3cft.com>
2011-06-06 18:03:52 +0100

    fixed url order and imported libraries

--- realales/brewery/urls.py
+++ realales/brewery/urls.py
@@ -1,14 +1,10 @@
-from django.conf.urls.defaults import *
-from django.views.generic import DetailView, ListView
+from django.conf.urls.defaults import url, patterns
+from django.views.generic import DetailView
 from brewery.models import Brewery
 
 urlpatterns = patterns('',
     url(r'^$', 'brewery.views.search', {'phrase':'', 'startswith':False}),
+    url(r'^(?P<pk>\d+)/$',DetailView.as_view(model=Brewery, template_name='brewery/detail.html'), name='brewery_detail'),
     url(r'^(?P<phrase>\w+)/$', 'brewery.views.search', {'startswith':False}, name='brewery_list'),
     url(r'^-(?P<phrase>\w+)/$', 'brewery.views.search', {'startswith':True}, name='brewery_startswith'),
-    url(r'^(?P<pk>\d+)/$',
-        DetailView.as_view(
-            model=Brewery,
-            template_name='brewery/detail.html'),
-        name='brewery_detail'),
 )
