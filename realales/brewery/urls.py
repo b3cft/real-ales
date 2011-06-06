11aab870797c3705aca087145508af02149452d0
Andrew "Bob" Brockhurst <andy.brockhurst@b3cft.com>
2011-06-06 07:42:01 +0100

    added brewery partial match and startswith urls

--- realales/brewery/urls.py
+++ realales/brewery/urls.py
@@ -3,10 +3,9 @@ from django.views.generic import DetailView, ListView
 from brewery.models import Brewery
 
 urlpatterns = patterns('',
-    url(r'^$', 'brewery.views.list', 
-        name='brewery_list'),
-    url(r'^find$', 'brewery.views.list',
-        name='brewery_search'),
+    url(r'^$', 'brewery.views.search', {'phrase':'', 'startswith':False}),
+    url(r'^(?P<phrase>\w+)/$', 'brewery.views.search', {'startswith':False}, name='brewery_list'),
+    url(r'^-(?P<phrase>\w+)/$', 'brewery.views.search', {'startswith':True}, name='brewery_startswith'),
     url(r'^(?P<pk>\d+)/$',
         DetailView.as_view(
             model=Brewery,
--- realales/brewery/views.py
+++ realales/brewery/views.py
@@ -1,28 +1,15 @@
 from beer.models import Beer
 from brewery.models import Brewery
+from django.template import RequestContext
 from django.core.paginator import Paginator, EmptyPage
 from django.shortcuts import render_to_response, get_object_or_404
 
-def list(request):
-    if request.GET.get('q'):
-        all_breweries = Brewery.objects.filter(name__contains=request.GET.get('q'))
+def search(request, phrase, startswith):
+    if startswith:
+        found_breweries = Brewery.objects.filter(name__startswith=request.GET.get('q', phrase))
     else:
-        all_breweries = Brewery.objects.all()
-        
-    paginator = Paginator(all_breweries, 25)
+        found_breweries = Brewery.objects.filter(name__contains=request.GET.get('q', phrase))
 
-    page = request.GET.get('page')
-    try:
-        breweries = paginator.page(page)
-    except TypeError:
-        breweries = paginator.page(1)
-    except EmptyPage:
-        breweries = paginator.page(paginator.num_pages)
-
-    return render_to_response('brewery/list.html', {"breweries": breweries})
-
-def search(request):
-    found_breweries = Brewery.objects.filter(name__contains=request.GET.get('q'))
     paginator = Paginator(found_breweries, 25)
 
     page = request.GET.get('page')
@@ -33,9 +20,9 @@ def search(request):
     except EmptyPage:
         breweries = paginator.page(paginator.num_pages)
 
-    return render_to_response('brewery/list.html', {"breweries": breweries})
+    return render_to_response('brewery/list.html', {"breweries": breweries}, context_instance=RequestContext(request))
 
 
 def detail(request, brewery_id):
     brewery = get_object_or_404(Brewery, pk=brewery_id)
-    return render_to_response('brewery/detail.html', {'brewery': brewery})
+    return render_to_response('brewery/detail.html', {'brewery': brewery}, context_instance=RequestContext(request))
