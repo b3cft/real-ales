db3e227a2bec4f5f3ea670caa85aa9e27f1343bd
Andrew "Bob" Brockhurst <andy.brockhurst@b3cft.com>
2011-06-07 23:33:25 +0100

    added email and mobile to brewery

--- realales/brewery/models.py
+++ realales/brewery/models.py
@@ -11,7 +11,9 @@ class Brewery(models.Model):
     long        = models.FloatField(null=True, blank=True)
     description = models.TextField(null=True, blank=True)
     website     = models.CharField(max_length=200,null=True, blank=True)
+    email       = models.CharField(max_length=200,null=True, blank=True)
     phone       = models.CharField(max_length=20,null=True, blank=True)
+    mobile      = models.CharField(max_length=20,null=True, blank=True)
     fax         = models.CharField(max_length=20,null=True, blank=True)
     active      = models.BooleanField()
 
